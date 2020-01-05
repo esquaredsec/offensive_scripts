5433430bb8179b7afdb5a073ab9f1f84b5f9424e
E Cheeks <16594866+esquaredsec@users.noreply.github.com>
2020-01-05 17:40:56 -0600

    Update zonetrans.sh

--- zonetrans.sh
+++ zonetrans.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Simple Zone Transfer Bash Script
-# $1 is the first argument given after the basah script
-# Check if argument was given, if not, print usage
+# $1 is the first argument given after the bash script.
+# Check if argument was given, if not, print usage.
 
 if [ -z "$1" ]; then
 	echo "[*] Simple Zone transfer script"
@@ -9,8 +9,8 @@ if [ -z "$1" ]; then
 	exit 0
 fi
 
-# If arguemtn was given, identify the DNS servers for the domain.
-# For each of these servers, attempt a zone transfer
+# If arguement was given, identify the DNS servers for the domain.
+# For each of these servers, attempt a zone transfer.
 
 for server in $(host -t ns $1 |cut -d" " -f4);do
 	host -l $1 $server |grep "has address"
